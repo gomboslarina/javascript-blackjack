fb6d3ec1b9605ea41dddc6a5cd539da8cbb663c3
Larina Gombos <gombos.larina@gmail.com>
2019-04-10 13:20:27 +0200

    game grid

--- server.py
+++ server.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template("index.html")
 
+
+@app.route('gameplay')
+def black_jack():
+    return render_template("game.html")
 
 if __name__ == '__main__':
     app.run()
